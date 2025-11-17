4216b6a141e53c251e3ab13d0a6252c30d132b26
Viacheslav Khoroshenko <slavahoroshenko@gmail.com>
2025-11-17 16:42:19 +0200

    Resolved task

---
+++ src/js/2-form.js
@@ -0,0 +1,47 @@
+const formData = {
+  email: '',
+  message: '',
+};
+const feedbackForm = document.querySelector('.feedbackForm');
+
+const storageKey = 'data';
+
+const savedData = localStorage.getItem(storageKey);
+
+if (savedData) {
+  const parsedData = JSON.parse(savedData);
+
+  formData.email = parsedData.email || '';
+  formData.message = parsedData.message || '';
+
+  feedbackForm.email.value = formData.email;
+  feedbackForm.message.value = formData.message;
+}
+
+const inputHandler = event => {
+  const target = event.target.name;
+
+  if (target === 'email' || target === 'message') {
+    formData[target] = event.target.value.trim();
+
+    localStorage.setItem(storageKey, JSON.stringify(formData));
+  }
+};
+
+const submitHandler = event => {
+  event.preventDefault();
+
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(localStorage.getItem(storageKey));
+
+  localStorage.removeItem(storageKey);
+  
+  feedbackForm.reset();
+};
+
+feedbackForm.addEventListener('input', inputHandler);
+feedbackForm.addEventListener('submit', submitHandler);
